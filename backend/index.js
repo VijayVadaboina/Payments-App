7add19081fa8f0763ef7fbbd95e0226e0da40d06
VijayVadaboina <vijayvvadaboina@gmail.com>
2025-01-15 00:35:17 +0530

    added cors and body-parser dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const userRouter = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors);
 app.use(express.json());
 app.use("/api/v1", userRouter);
 
