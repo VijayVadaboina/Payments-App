f1ee545ed0b22accc16e5a0456e555149b5c5a56
VijayVadaboina <vijayvvadaboina@gmail.com>
2025-01-15 22:34:13 +0530

    configured dotenv for the backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const userRouter = require("./routes/index");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
-
+const port = process.env.PORT || 5000;
 app.use(cors);
 app.use(express.json());
 app.use("/api/v1", userRouter);
 
-app.listen(3000, () => {
-  console.log(" server started on port 3000");
+app.listen(port, () => {
+  console.log(`server started on port {port}`);
 });
